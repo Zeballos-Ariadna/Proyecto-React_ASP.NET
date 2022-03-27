31269b36da8064df40bbbee7192f9c1a458ce3ee
Ariadna Zeballos <zeballosariadna973@gmail.com>
2022-03-26 22:10:38 -0300

    Texto con estilo con Markdown

--- src/actores/EditarActores.tsx
+++ src/actores/EditarActores.tsx
@@ -6,6 +6,8 @@ export default function EditarActores(){
             <h3>Editar Actor</h3>
             <FormularioActores 
                 modelo={{nombre: 'Tom Holland', 
+                        biografia: `# tom 
+                        Ha nacido **tom**` ,
                         fechaNacimiento: new Date('1996-06-01T00:00:00'),
                         fotoURL: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Tom_Holland_by_Gage_Skidmore.jpg'
                 }}
--- src/actores/FormularioActores.tsx
+++ src/actores/FormularioActores.tsx
@@ -6,6 +6,7 @@ import { actorCreacionDTO } from "./actores.model";
 import * as Yup from 'yup'
 import FormGroupFecha from "../utils/FormGroupFecha";
 import FormGroupImagen from "../utils/FormGroupImagen";
+import FormGroupMarkdown from "../utils/FormGroupMarkdown";
 
 export default function FormularioActores(props: formularioActoresProps){
     return(
@@ -21,7 +22,8 @@ export default function FormularioActores(props: formularioActoresProps){
                     <FormGroupText campo="nombre" label="Nombre"/>
                     <FormGroupFecha label="Fecha Nacimiento" campo="fechaNacimiento"/>
                     <FormGroupImagen campo="foto" label="Foto" imagenURL={props.modelo.fotoURL}/>
-
+                    <FormGroupMarkdown campo="biografia" label="Biografia" />
+                    
                     <Button disabled={formikProps.isSubmitting}
                         type="submit"
                     >Salvar</Button>
