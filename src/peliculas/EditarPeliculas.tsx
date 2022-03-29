ab79aaf2b38652d38cdbf37f1588513f4a137eb1
Ariadna Zeballos <zeballosariadna973@gmail.com>
2022-03-28 21:43:24 -0300

    Selector de Géneros

--- src/peliculas/EditarPeliculas.tsx
+++ src/peliculas/EditarPeliculas.tsx
@@ -1,10 +1,22 @@
+import { generoDTO } from "../generos/generos.model";
 import FormularioPeliculas from "./FormularioPeliculas";
 
 export default function EditarPeliculas(){
+    const generosNoSeleccionados: generoDTO[] = [
+        {id:2, nombre: 'Drama'}
+    ]
+
+    const generosSeleccionados: generoDTO[] = [
+        {id:1, nombre: 'Acción'},
+        {id:3, nombre: 'Comedia'}
+    ]
+
     return(
         <>
             <h3>Editar Película</h3>
             <FormularioPeliculas 
+                generosNoSeleccionados={generosNoSeleccionados}
+                generosSeleccionados={generosSeleccionados}
                 modelo={{titulo:'SpiderMan', enCines: true, trailer:'url',
                     fechaLanzamiento: new Date('2019-01-01T00:00:00')
                 }}
