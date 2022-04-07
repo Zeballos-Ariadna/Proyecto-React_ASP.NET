01054bd3226e63be694e2a7c0a70f4067e51beee
Ariadna Zeballos <zeballosariadna973@gmail.com>
2022-04-07 00:26:22 -0300

    Instalé Axios y agregué Variables de ambiente

--- src/generos/IndiceGeneros.tsx
+++ src/generos/IndiceGeneros.tsx
@@ -1,6 +1,16 @@
+import axios, { AxiosResponse } from "axios";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { urlGeneros } from "../utils/endpoints";
+import { generoDTO } from "./generos.model";
 
 export default function IndiceGeneros(){
+    useEffect(() => {
+        axios.get(urlGeneros)
+            .then((respuesta: AxiosResponse<generoDTO[]>) => {
+                console.log(respuesta.data);
+            })
+    }, [])//arreglo de dependencias vacío
     return(
         <>
             <h3>Indice Géneros</h3>
