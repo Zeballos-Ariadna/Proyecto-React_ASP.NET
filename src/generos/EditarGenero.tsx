f1eb7f12d1280b1a9acade85b0893d3fc1a3c44b
Ariadna Zeballos <zeballosariadna973@gmail.com>
2022-04-11 16:17:20 -0300

    Generalizando el Componente de Indice

--- src/generos/EditarGenero.tsx
+++ src/generos/EditarGenero.tsx
@@ -1,44 +1,24 @@
-import axios, { AxiosResponse } from "axios";
-import { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
-import Cargando from "../utils/Cargando";
+import EditarEntidad from "../utils/EditarEntidad";
 import { urlGeneros } from "../utils/endpoints";
 import FormularioGeneros from "./FormularioGeneros";
 import { generoCreacionDTO, generoDTO } from "./generos.model";
 
 export default function EditarGenero(){
-    const {id}: any= useParams();
-    const [genero,setGenero] = useState<generoDTO>();
-    const [errores,setErrores] = useState<string[]>([]);
-    const history= useHistory();
-
-    useEffect(() => {
-        axios.get(`${urlGeneros}/${id}`)
-        .then((respuesta: AxiosResponse<generoDTO>) => {
-            setGenero(respuesta.data);
-        })
-    })
-
-    async function editar(generoEditar: generoCreacionDTO){
-        try{
-            await axios.put(`${urlGeneros}/${id}`, generoEditar);
-            history.push('/generos');
-        }
-        catch(error){
-            setErrores(error.response.data)
-        }
-    }
-
+    
     return(
         <>
-            <h3>Editar Género</h3>
-            {genero ? 
-                <FormularioGeneros modelo={genero} 
-                onSubmit={async valores=> {
-                    await editar(valores)
-                }}
-            /> :  <Cargando />}
-            
+            <EditarEntidad<generoCreacionDTO, generoDTO>
+                url={urlGeneros} urlIndice= "/generos" nombreEntidad="Generos"
+                >
+                {(entidad, editar) => 
+                    <FormularioGeneros modelo={entidad} 
+                        onSubmit={async valores=> {
+                        await editar(valores)
+                        }}
+                    />
+                }
+            </EditarEntidad>
+                           
         </>
         
     )
--- src/generos/IndiceGeneros.tsx
+++ src/generos/IndiceGeneros.tsx
@@ -1,102 +1,41 @@
-import axios, { AxiosResponse } from "axios";
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Button from "../utils/Button";
 import { urlGeneros } from "../utils/endpoints";
-import ListadoGenerico from "../utils/ListadoGenerico";
-import Paginacion from "../utils/Paginacion";
+import IndiceEntidad from "../utils/IndiceEntidad";
 import { generoDTO } from "./generos.model";
-import confirmar from './../utils/Confirmar';
 
 export default function IndiceGeneros(){
-
-    const[generos, setGeneros]= useState<generoDTO[]>();
-    const [totalDePaginas, setTotalDePaginas]= useState(0);
-    const [recordsPorPagina,setRecordsPorPagina]= useState(10);
-    const [pagina, setPagina]= useState(1);
-
-
-    useEffect(() => {
-        cargarDatos();
-    }, [pagina, recordsPorPagina])//arreglo de dependencias vacío
-    
-    function cargarDatos(){
-        axios.get(urlGeneros, {
-            params: {pagina, recordsPorPagina}
-        })
-            .then((respuesta: AxiosResponse<generoDTO[]>) => {
-                const totalDeregistros= 
-                parseInt(respuesta.headers['cantidadtotalregistros'],10);
-                setTotalDePaginas(Math.ceil(totalDeregistros/recordsPorPagina));
-                console.log(respuesta.data);
-                setGeneros(respuesta.data);
-            })
-    }
-
-    async function borrar(id: number){
-        try{
-            await axios.delete(`${urlGeneros}/${id}`)
-            cargarDatos();
-        }
-        catch(error){
-            console.log(error.response.data);
-        }
-    }
     
     return(
         <>
-            <h3>Indice Géneros</h3>
-            <Link className="btn btn-primary" to="generos/crear">Crear Género</Link>
+        <IndiceEntidad<generoDTO>
+            url={urlGeneros}
+            urlCrear="generos/crear"
+            titulo="Géneros"
+            nombreEntidad="Género"
+        >
+        {(generos, botones) => <>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Nombre</th>
+                </tr>
+            </thead>
+            <tbody>
+                {generos?.map(genero => 
+                <tr key = {genero.id}>
+                    <td>
+                        {botones(`generos/editar/${genero.id}`, genero.id)}
+                    </td>
+                    <td>
+                        {genero.nombre}
+                    </td>
+                </tr>)}
+            </tbody>
+        </>}
+        </IndiceEntidad>    
 
-            <div className="form-group" style={{width: '150px'}}>
-                <label>Registros por páginas:</label>
-                <select 
-                    className="form-control"
-                    defaultValue={10}
-                    onChange={e => {
-                    setPagina(1);
-                    setRecordsPorPagina(parseInt(e.currentTarget.value,10))
-                    }}> 
-                    <option value={5}>5</option>
-                    <option value={10}>10</option>
-                    <option value={25}>25</option>
-                    <option value={50}>50</option>
-                </select>
-            </div>
-
-            <Paginacion cantidadTotalDePaginas={totalDePaginas}
-            paginaActual={pagina} onChange={nuevaPagina => setPagina(nuevaPagina)}/>
-
-            <ListadoGenerico listado={generos}>
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Nombre</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {generos?.map(genero => 
-                        <tr key = {genero.id}>
-                            <td>
-                                <Link className="btn btn-success" to={`/generos/editar/${genero.id}`}>
-                                    Editar
-                                </Link>
-                                <Button onClick={() => confirmar(() => borrar(genero.id)) }
-                                className= "btn btn-danger"
-                                >Borrar</Button>
-
-                            </td>
-                            <td>
-                                {genero.nombre}
-                            </td>
-                        </tr>)}
-                    </tbody>
-                </table>
-            </ListadoGenerico>
-
-            <br></br>
-            <Link to="generos/editar">Editar Género</Link>
+            {/*<br></br>
+            <Link to="generos/editar">Editar Género</Link>*/}
         </>
         
     )
