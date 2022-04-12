cc37b545f166de4d5183be430b20d2afed0eca39
Ariadna Zeballos <zeballosariadna973@gmail.com>
2022-04-12 14:18:48 -0300

    Crear Actores

--- src/actores/CrearActores.tsx
+++ src/actores/CrearActores.tsx
@@ -1,12 +1,32 @@
+import axios from "axios";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { urlActores } from "../utils/endpoints";
+import MostrarErrores from "../utils/MostrarErrores";
+import { actorCreacionDTO } from "./actores.model";
 import FormularioActores from "./FormularioActores";
 
 export default function CrearActores(){
+    const [errores,setErrores] =useState<string[]>([]);
+    const history= useHistory();
+
+    async function crear(actor: actorCreacionDTO){
+        try{
+            await axios.post(urlActores, actor);
+            history.push('/actores');
+        }
+        catch(error){
+            setErrores(error.response.data);
+        }
+    }
+
     return(
         <>
             <h3>Crear Actor</h3>
+            <MostrarErrores errores={errores} />
             <FormularioActores 
                 modelo={{nombre: '', fechaNacimiento: undefined}}
-                onSubmit={valores => console.log(valores)}
+                onSubmit={async valores => await crear(valores)}
             />
         </>
         
