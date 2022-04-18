18e85e06fa721bb7b9b67741711cf3cb224e08e5
Ariadna Zeballos <zeballosariadna973@gmail.com>
2022-04-18 10:39:55 -0300

    Crear Cine

--- src/cines/CrearCines.tsx
+++ src/cines/CrearCines.tsx
@@ -1,12 +1,34 @@
+import axios from "axios";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { urlCines } from "../utils/endpoints";
+import MostrarErrorCampo from "../utils/MostrarErrorCampo";
+import MostrarErrores from "../utils/MostrarErrores";
+import { cineCreacionDTO } from "./cines.model";
 import FormulariosCines from "./FormulariosCines";
 
 export default function CrearCines(){
+
+    const history =useHistory();
+    const [errores,setErrores]=useState<string[]>([]);
+
+    async function crear(cine: cineCreacionDTO){
+        try{
+            await axios.post(urlCines, cine);
+            history.push('/cines');
+        }
+        catch(error){
+            setErrores(error.response.data);
+        }
+    }
+
     return(
         <>
             <h3>Crear Cine</h3>
+            <MostrarErrores errores={errores}/>
             <FormulariosCines 
                 modelo={{nombre: ''}}
-                onSubmit={valores => console.log(valores)}
+                onSubmit={async valores => await crear(valores)}
             />
         </>
         
