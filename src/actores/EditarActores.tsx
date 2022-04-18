418a4f9e93e76e3c2fefb9d12dab434028ef9f53
Ariadna Zeballos <zeballosariadna973@gmail.com>
2022-04-18 10:39:29 -0300

    Editar Actores

--- src/actores/EditarActores.tsx
+++ src/actores/EditarActores.tsx
@@ -1,13 +1,26 @@
 import EditarEntidad from "../utils/EditarEntidad";
 import { urlActores } from "../utils/endpoints";
+import { convertirActorAFormData } from "../utils/FormDataUtils";
 import { actorCreacionDTO, actorDTO } from "./actores.model";
 import FormularioActores from "./FormularioActores";
 
 export default function EditarActores(){
+
+    const transformar = (actor: actorDTO) => {
+        return{
+            nombre: actor.nombre,
+            fotoURL: actor.foto,
+            biografia: actor.biografia,
+            fechaNacimiento: new Date(actor.fechaNacimiento)
+        }
+    }
+
     return(
         <>
             <EditarEntidad<actorCreacionDTO, actorDTO>
                 url={urlActores} urlIndice= "/actores" nombreEntidad="Actores"
+                transformarFormData={convertirActorAFormData}
+                transformar={transformar}
                 >
                 {(entidad, editar) => 
                     <FormularioActores 
