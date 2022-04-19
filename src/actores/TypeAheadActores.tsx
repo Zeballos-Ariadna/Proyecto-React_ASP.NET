571ae22859393b181e214911ff2b94559608bf5b
Ariadna Zeballos <zeballosariadna973@gmail.com>
2022-04-19 11:29:00 -0300

    Selector de Actores

--- src/actores/TypeAheadActores.tsx
+++ src/actores/TypeAheadActores.tsx
@@ -1,28 +1,28 @@
+import axios, { AxiosResponse } from "axios";
 import { ReactElement, useState } from "react";
-import { Typeahead } from "react-bootstrap-typeahead";
+import { AsyncTypeahead } from "react-bootstrap-typeahead";
+import { urlActores } from "../utils/endpoints";
 import { actorPeliculaDTO } from "./actores.model";
 
 export default function TypeAheadActores(props: typeAheadActores){
     
-    const actores: actorPeliculaDTO[] = [
-        {
-            id:1, nombre: 'Tom Holland', personaje:'Spider-Man', 
-            foto:'https://upload.wikimedia.org/wikipedia/commons/3/3c/Tom_Holland_by_Gage_Skidmore.jpg'
-        },
-        {
-            id:2, nombre: 'Fernando',personaje: '',
-            foto: 'https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_UX214_CR0,0,214,317_AL_.jpg'
-        },
-        {
-            id:3, nombre: 'Mariano',personaje: '',
-            foto: 'https://m.media-amazon.com/images/M/MV5BMjI0MTg3MzI0M15BMl5BanBnXkFtZTcwMzQyODU2Mw@@._V1_UY317_CR10,0,214,317_AL_.jpg'
-        }
-    ];
-
     const seleccion: actorPeliculaDTO[] = []
 
     const[elementoArrastrado,setElementoArrastrado] =
         useState<actorPeliculaDTO | undefined>(undefined)
+
+    const [estaCargando, setEstaCargando] = useState(false);
+    const [opciones, setOpciones] = useState<actorPeliculaDTO[]>([]);
+
+    function manejarBusqueda(query: string){
+        setEstaCargando(true);
+        
+        axios.get(`${urlActores}/buscarPorNombre/${query}`)
+        .then((respuesta: AxiosResponse<actorPeliculaDTO[]>) =>{
+            setOpciones(respuesta.data);
+            setEstaCargando(false);
+        })
+    }
 
     function manejarDragStart (actor: actorPeliculaDTO){
         setElementoArrastrado(actor);
@@ -46,16 +46,18 @@ export default function TypeAheadActores(props: typeAheadActores){
     return(
         <>
             <label>Actores</label>
-            <Typeahead 
+            <AsyncTypeahead 
                 id="typeahead"
                 onChange={actores =>{
                     if(props.actores.findIndex(x => x.id === actores[0].id) === -1){
                         props.onAdd([...props.actores, actores[0]]);
                     }
                 }}
-                options={actores}
+                options={opciones}
                 labelKey={actor=>actor.nombre}
-                filterBy={['nombre']}
+                filterBy={() => true}
+                isLoading={estaCargando}
+                onSearch={manejarBusqueda}
                 placeholder="Escriba el nombre del actor"
                 minLength={2}
                 flip={true}
