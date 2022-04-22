057ba1c9a900366e4ca3d3b8bb16ab043fd6ea35
Ariadna Zeballos <zeballosariadna973@gmail.com>
2022-04-22 11:15:18 -0300

    Rutas protegidas-Acceso Admin, Seguridad

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,19 @@ configurarValidaciones();
 function App() {
   const [claims,setClaims] = useState<claim[]>([
     {nombre: 'email', valor:'felipe@hotmail.com'},
-    //{nombre: 'role', valor:'admin'}
+    {nombre: 'role', valor:'admin'}
 
   ]);
 
   function actualizar(claims: claim[]){
     setClaims(claims);
+  }
+
+  //si el usuario tiene un claim de nombre role y valor admin
+  //entonces el usuario es admin, de lo contrario no lo es
+  function esAdmin(){
+    return claims.findIndex(claim => 
+      claim.nombre === 'role' && claim.valor === 'admin') > -1;
   }
 
   return (
@@ -31,7 +38,10 @@ function App() {
             <Switch>
               {rutas.map(ruta => <Route key={ruta.path} 
                 path={ruta.path} exact={ruta.exact}>
-                  <ruta.componente />
+                  {ruta.esAdmin && !esAdmin() ? <>
+                    No tiene permiso para acceder al componente
+                  </> : <ruta.componente />}
+                  
               </Route>)}
             </Switch> 
           </div>
